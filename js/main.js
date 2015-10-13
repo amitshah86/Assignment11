202c7188dfe1b1ca2d7f24f17a20d77bdf967c64
Amit Shah <ashah325i@gmail.com>
2015-10-12 20:36:25 -0400

    worked on JS functions and HTML

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
- 
- ( function () {
+(function(){
+
+var pb = $("#itemTemplate").html();
+var render = _.template(pb);
+
+window.Spencer = {
+  fullName: "Spencer Wyckoff",
+  age: 27,
+  TIYGraduate: true,
+  Shoe: {
+    size: 13,
+    laces: true,
+  }
+};
+
+_.each(etsy.results, function(result) {
+  console.log(result);
+  var itemHTML = render(result);
+  console.log(itemHTML);
+  $('.rightContainer').append(itemHTML);
+});
 
 
 
-
- }
+})();
